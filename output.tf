8210ff9c423abb23eca65ac2f574f941529282e9
Jeffrey Monte <46807283+jmonte-sph@users.noreply.github.com>
2022-07-29 11:32:49 +0800

    fixed output fmt

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "codebuild_output_artifact_name" {
   description = "codebuild output artifact name that can be use for the next stage(if any)"
-  value = local.codebuild_output_artifact_name
+  value       = local.codebuild_output_artifact_name
 }
